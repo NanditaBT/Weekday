2e853296b631d1757252558dd082686fceaabbab
Nandita <Hp@a>
2017-06-11 18:29:37 +0530

    sql

--- src/generics/SQLConnection.java
+++ src/generics/SQLConnection.java
@@ -2,6 +2,7 @@ package generics;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -12,13 +13,25 @@ public class SQLConnection {
 	
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		
+		Connection con=null;
+		try{
 		Class.forName("com.mysql.jdbc.Driver");
 		System.out.println("loaded");
 		
-Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3036/emp");
+ con=DriverManager.getConnection("jdbc:mysql://localhost:3036/selenium","root","hellosql");
+System.out.println("connected to db");
+		}
+		catch(Exception e)
+		{
+			System.out.println("could not connect to database");
+			//throw new SkipException("Could not establish connection"); 
+		}
+
 
      Statement stmt = con.createStatement(); 
-     ResultSet rs = stmt.executeQuery("select * from employee");
+   PreparedStatement pstmt = con.prepareStatement(sql);
+   pstmt.setString(parameterIndex, x);
+//     ResultSet rs = stmt.executeQuery("select * from seleniumuser");
 
 //while(rs.next())
 //{
